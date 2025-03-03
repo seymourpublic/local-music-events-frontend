9a7e8377a88680b31cb54aa544701d7bd3d2cccd
Naledi Sandamela <56083410+seymourpublic@users.noreply.github.com>
2025-03-03 19:31:27 +0200

    Refactor Navbar component and add styling for improved layout and user experience

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
-    return (
-        <nav style={{ padding: '1rem', background: '#f4f4f4' }}>
-          <Link to="/">Home</Link> |{' '}
-          <Link to="/login">Login</Link> |{' '}
-          <Link to="/register">Register</Link>
-        </nav>
-      );
-    };
-    
-    export default Navbar;
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <button className="home-button">Home</button>
+        <span className="navbar-text">good</span>
+      </div>
+      <div className="navbar-right">
+        <img
+          src="https://via.placeholder.com/40"
+          alt="User Avatar"
+          className="user-avatar"
+        />
+        <div className="user-info">
+          <div className="username">Username</div>
+          <div className="description">Description</div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
